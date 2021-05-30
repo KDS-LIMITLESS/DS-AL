ad3e17cf313e8caebe7c3309a8db60d9b84d0de3
KDS-Limitless <limitlesskds@gmail.com>
2021-05-30 17:52:38 +0100

    AlgorithmFriday week 8

--- AlgorithmFridays/week 6 excercise.py
+++ AlgorithmFridays/week 6 excercise.py
@@ -2,12 +2,11 @@ words = {1: "", 2: "abc", 3: "def", 4: "ghi", 5: "jkl", 6: "mno",
         7: "pqrs", 8: "tuv", 9: "wxyz"}
 
 
-def getletters(number: int):
+def compute_combinations(number: int):
 	counter = 0
 	for nums in  str(number):
 		if counter == 0:
 			v = words[int(nums)]
-			print([v])
 			counter += 1
 			continue
 		m = words[int(nums)]
@@ -15,6 +14,4 @@ def getletters(number: int):
 			for i in m:
 				print([l+i])
 		
-	
-    
-getletters(234)
+compute_combinations(23)
